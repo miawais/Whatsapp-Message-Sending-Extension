e0df43fd49f35ddc8d5c000642cbaedf289612f4
miawais <awaisbinmukhtar@gmail.com>
2024-07-13 00:51:04 +0500

    code updated for login api error

--- app.py
+++ app.py
@@ -1,61 +1,22 @@
 from flask import Flask, render_template, request, jsonify
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 app = Flask(__name__)
 
-driver = None
+logged_in = False
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-@app.route('/login', methods=['POST'])
-def login():
-    global driver
-    driver = webdriver.Chrome()
-    driver.get("https://web.whatsapp.com/")
-    return jsonify(status='waiting')
-
-@app.route('/check_login', methods=['POST'])
-def check_login():
-    try:
-        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, "canvas[aria-label='Scan me!']")))
-        return jsonify(status='waiting')
-    except Exception:
-        return jsonify(status='logged_in')
-
-@app.route('/send_message', methods=['POST'])
-def send_message():
-    data = request.get_json()
-    group_names = data['groups']
-    message = data['message']
-
-    for name in group_names:
-        search_box = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="side"]/div[1]/div/div[2]/button'))
-        )
-        search_box.click()
-        search_input = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="side"]/div[1]/div/div[2]/div[2]/div/div[1]/p'))
-        )
-        search_input.send_keys(name + Keys.ENTER)
-        time.sleep(2)
-        message_box = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'))
-        )
-        message_box.send_keys(message)
-        send_button = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button/span'))
-        )
-        send_button.click()
-        time.sleep(3)
-    
-    return jsonify(status='success')
+    global logged_in
+    if request.method == 'POST':
+        if 'login' in request.form:
+            logged_in = True
+            return jsonify({'success': True})
+        elif 'send_message' in request.form and logged_in:
+            group_names = request.form.get('group_names')
+            message = request.form.get('message')
+            # Add logic to send messages to the specified groups
+            return jsonify({'success': True, 'message': 'Messages sent successfully!'})
+    return render_template('index.html', logged_in=logged_in)
 
 if __name__ == '__main__':
     app.run(debug=True)
